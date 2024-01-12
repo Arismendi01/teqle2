8611d67a9b086fac50c6b9c0adca9c376ecd604b
arismendi01 <arismendicj@gmail.com>
2024-01-11 23:43:13 -0300

    Mejore la parte de servicios y mejore la parte de opiniones

--- java.js
+++ java.js
@@ -44,3 +44,92 @@ var swiper = new Swiper('.swiper-container', {
 	},
 	speed: 800, // Ajusta la velocidad de transición según sea necesario
   });
+
+  //este es el titulo de las secciones
+  class ScrambleTheText {
+    constructor(getTheTitle, chars) {
+        this.getTheTitle = getTheTitle;
+        this.chars = chars;
+        this.update = this.update.bind(this);
+    }
+
+    setText(newText) {
+        var oldText = this.getTheTitle.innerText;
+        var length = Math.max(oldText.length, newText.length);
+        var promise = new Promise((resolve) => (this.resolve = resolve));
+
+        this.queue = [];
+        for (let i = 0; i < length; i++) {
+            var from = oldText[i] || "";
+            var to = newText[i] || "";
+            var start = Math.floor(Math.random() * 40);
+            var end = start + Math.floor(Math.random() * 40);
+            this.queue.push({
+                from,
+                to,
+                start,
+                end
+            });
+        }
+        cancelAnimationFrame(this.frameRequest);
+        this.frame = 0;
+        this.update();
+        return promise;
+    }
+
+    update() {
+        let output = "";
+        let complete = 0;
+        for (let i = 0, n = this.queue.length; i < n; i++) {
+            let { from, to, start, end, char } = this.queue[i];
+            if (this.frame >= end) {
+                complete++;
+                output += to;
+            } else if (this.frame >= start) {
+                if (!char || Math.random() < 0.28) {
+                    char = this.randomCharacter();
+                    this.queue[i].char = char;
+                }
+                output += `<span class="dummy">${char}</span>`;
+            } else {
+                output += from;
+            }
+        }
+        this.getTheTitle.innerHTML = output;
+        if (complete === this.queue.length) {
+            this.resolve();
+        } else {
+            this.frameRequest = requestAnimationFrame(this.update);
+            this.frame++;
+        }
+    }
+
+    randomCharacter() {
+        return this.chars[Math.floor(Math.random() * this.chars.length)];
+    }
+}
+
+// The Titles
+let serviciosTitle = "Nuestros Servicios";
+let opinionesTitle = "Opiniones";
+
+let getServiciosTitle = document.getElementById("servicios-section").querySelector(".title");
+let getOpinionesTitle = document.getElementById("opiniones-section").querySelector(".opiniones");
+
+let ServiciosTitleEffect = new ScrambleTheText(getServiciosTitle, serviciosTitle);
+let OpinionesTitleEffect = new ScrambleTheText(getOpinionesTitle, opinionesTitle);
+
+let nextServiciosTitle = () => {
+    ServiciosTitleEffect.setText(serviciosTitle).then(() => {
+        setTimeout(nextServiciosTitle, 1500);
+    });
+};
+
+let nextOpinionesTitle = () => {
+    OpinionesTitleEffect.setText(opinionesTitle).then(() => {
+        setTimeout(nextOpinionesTitle, 1500);
+    });
+};
+
+nextServiciosTitle();
+nextOpinionesTitle();

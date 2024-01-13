7ac43a8a4edec39807a88c0986fc73a6ff468e1f
arismendi01 <arismendicj@gmail.com>
2024-01-12 21:44:14 -0300

    agregue la seccion de clientes

--- java.js
+++ java.js
@@ -133,3 +133,36 @@ let nextOpinionesTitle = () => {
 
 nextServiciosTitle();
 nextOpinionesTitle();
+
+
+// Configuración del carrusel de marcas seccion
+const carrusel = document.querySelector(".carrusel-items");
+
+let maxScrollLeft = carrusel.scrollWidth - carrusel.clientWidth;
+let intervalo = null;
+let step = 1;
+const start = () => {
+    intervalo = setInterval(function () {
+        carrusel.scrollLeft = carrusel.scrollLeft + step;
+        if (carrusel.scrollLeft === maxScrollLeft) {
+            step = step * -1;
+        } else if (carrusel.scrollLeft === 0) {
+            step = step * -1;
+        }
+    }, 10);
+};
+
+const stop = () => {
+    clearInterval(intervalo);
+};
+
+carrusel.addEventListener("mouseover", () => {
+    stop();
+});
+
+carrusel.addEventListener("mouseout", () => {
+    start();
+});
+
+start();
+
